35d9f4773fed6af35d0164c8197f4cfc108f0720
Alan Calvillo <alan.calvillo@wizeline.com>
2021-06-06 20:19:47 -0500

    new gpstrack version

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -8,48 +8,46 @@ import { BackgroundMode } from '@ionic-native/background-mode';
 import './Tab2.css';
 const bg = BackgroundMode;
 bg.enable();
-const saveCoordinates = () => {
-  setInterval(function(){
-      navigator.geolocation.getCurrentPosition(function(position) {
-        let lat:any = position.coords.latitude.toString()
-        let long:any = position.coords.longitude.toString()
-        let cellphone: any=localStorage.getItem("cellphone")
-        let name: any=localStorage.getItem("name")
-        let email: any=localStorage.getItem("email")
-        let profile: any=localStorage.getItem("profile")
-        setLat(lat)
-        setLon(long)
-        setTime(moment().format('DD-MM-YYYY HH:mm:ss a').toString())
-        var formData = new FormData();
-        
-        formData.append("cellNumber", cellphone);
-        formData.append("name", name);
-        formData.append("email", email);
-        formData.append("profile", profile);
-        formData.append("lat", lat);
-        formData.append("long", long);
-        formData.append("date", moment().format('DD-MM-YYYY'));
-        formData.append("time", moment().format('HH:mm:ss a'));
-        //formData.append("cellNumber", localStorage.getItem("cellphone"));
-        axios.post('https://com-geored.uaa.mx/save', formData).then(response => {             
-          setLat(position.coords.latitude.toString())
-          setLon(position.coords.longitude.toString())
-          setTime(moment().format('DD-MM-YYYY HH:mm:ss a').toString())
-          console.log(response);        
-        });
-        
-    });
-  }, 1 * 60 * 100);
-  
-  
-}
+
 const Tab2: React.FC = () => {
   const [lat, setLat] = useState('N/A')  
   const [lon, setLon] = useState('N/A')  
   const [time, setTime] = useState('N/A')  
-  
-  
-  
+  const saveCoordinates = () => {
+    setInterval(function(){
+        navigator.geolocation.getCurrentPosition(function(position) {
+          let lat:any = position.coords.latitude.toString()
+          let long:any = position.coords.longitude.toString()
+          let cellphone: any=localStorage.getItem("cellphone")
+          let name: any=localStorage.getItem("name")
+          let email: any=localStorage.getItem("email")
+          let profile: any=localStorage.getItem("profile")
+          setLat(lat)
+          setLon(long)
+          setTime(moment().format('DD-MM-YYYY HH:mm:ss a').toString())
+          var formData = new FormData();
+          
+          formData.append("cellNumber", cellphone);
+          formData.append("name", name);
+          formData.append("email", email);
+          formData.append("profile", profile);
+          formData.append("lat", lat);
+          formData.append("long", long);
+          formData.append("date", moment().format('DD-MM-YYYY'));
+          formData.append("time", moment().format('HH:mm:ss a'));
+          //formData.append("cellNumber", localStorage.getItem("cellphone"));
+          axios.post('https://com-geored.uaa.mx/save', formData).then(response => {             
+            setLat(position.coords.latitude.toString())
+            setLon(position.coords.longitude.toString())
+            setTime(moment().format('DD-MM-YYYY HH:mm:ss a').toString())
+            console.log(response);        
+          });
+          
+      });
+    }, 1 * 60 * 100);
+    
+    
+  }
   return (
     <IonPage>
       <IonHeader>
